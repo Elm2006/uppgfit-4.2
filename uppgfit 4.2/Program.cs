bf75eda50b7f6b6530c4eceb07d83c442391c7c8
Elm2006 <143172014+Elm2006@users.noreply.github.com>
2023-10-13 13:40:15 +0200

    Add project files.

---
+++ uppgfit 4.2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace uppgift_4.__2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int tal=1495; tal >1399; tal-=5)
+            {
+                Console.WriteLine(tal);
+            }
+            Console.ReadKey();
+        }
+    }
+}
